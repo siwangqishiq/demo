8ab6e395c2ed1627d3ff7397a7df26c3a8e1f03f
panyi <525647740@qq.com>
2018-11-26 00:33:16 +0800

    Initial commit

---
+++ src/main/java/com/mingyan/demo/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.mingyan.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/hello")
+    public String index() {
+        return "Hello World 你好 公主殿下";
+    }
+}//end class
