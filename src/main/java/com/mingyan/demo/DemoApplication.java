5c4b041218e0a8f4175587a28d762ab262f07d71
panyi <525647740@qq.com>
2018-11-27 23:29:30 +0800

    commit test

--- src/main/java/com/mingyan/demo/DemoApplication.java
+++ src/main/java/com/mingyan/demo/DemoApplication.java
@@ -3,10 +3,14 @@ package com.mingyan.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Map;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+		Map s;
 	}
 }
